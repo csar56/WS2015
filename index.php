517887409520f012d8fa9cd005b926925bcf6a17
Christoph Sauer <info@sauer-chris.de>
2015-10-14 18:47:27 +0200

    Add PHP Support

--- index.php
+++ index.php
@@ -1,37 +1,12 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-	<meta charset="UTF-8" />
 	<title>SauerIT</title>
-	<link rel="shortcut icon" href="img/sauerit.ico" type="image/x-icon" /> 
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-	<script src="bootstrap/jquery.js"></script>
-	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<?php include('php/layout/head.php');?>
 </head>
 <body>
-    <nav role="navigation" class="navbar navbar-inverse">
-        <div class="navbar-header">
-            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a href="#" class="navbar-brand"><img src="img/logo.svg" title="Logo" style="margin-top:-11px;height:64px;"/></a>
-        </div>
-        <div id="navbarCollapse" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Profile</a></li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Meine Webseiten <b class="caret"></b></a>
-                    <ul role="menu" class="dropdown-menu">
-                        <li><a href="#">BuMs Fellingshausen</a></li>
-                        <li><a href="#">JF Fellingshausen</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </nav>
+	<?php include('php/layout/navbar.php');?>
+
 
 
 </body>
