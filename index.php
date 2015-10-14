0996e55970e2eaf45812312bce9c5cb8e6b55300
Christoph Sauer <info@sauer-chris.de>
2015-10-14 18:02:34 +0200

    Add Favicon and Navigation

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <!DOCTYPE html>
 <html lang="de">
-<head></head>
-<body></body>
+<head>
+	<meta charset="UTF-8" />
+	<title>SauerIT</title>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<script src="bootstrap/jquery.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <nav role="navigation" class="navbar navbar-inverse">
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#" class="navbar-brand">SauerIT</a>
+        </div>
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Profile</a></li>
+                <li class="dropdown">
+                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Meine Webseiten <b class="caret"></b></a>
+                    <ul role="menu" class="dropdown-menu">
+                        <li><a href="#">BuMs Fellingshausen</a></li>
+                        <li><a href="#">JF Fellingshausen</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
+</body>
 </html>
