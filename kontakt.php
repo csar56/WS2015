b612ba811907a0b61c92aa57eb870b522e54f5e8
Christoph Sauer <info@sauer-chris.de>
2015-10-14 18:48:12 +0200

    Add Kontakt Page

---
+++ kontakt.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<title>SauerIT</title>
+	<?php include('php/layout/head.php');?>
+</head>
+<body>
+	<?php include('php/layout/navbar.php');?>
+	<main class="container-fluid">
+		<div class="row">
+			<div class="col-md-8 col-md-offset-2">
+				<h1>Christoph Sauer</h1>
+				Rodheimerstraße 28
+				
+			</div>
+		</div>
+	</main>
+
+
+</body>
+</html>
