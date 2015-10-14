4bda215f472a22582a7c8fcaddb1dfd0c9619be5
Christoph Sauer <info@sauer-chris.de>
2015-10-14 18:04:13 +0200

    Add Logo as SVG

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>SauerIT</title>
+	<link rel="shortcut icon" href="img/sauerit.ico" type="image/x-icon" /> 
 	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 	<script src="bootstrap/jquery.js"></script>
 	<script src="bootstrap/js/bootstrap.min.js"></script>
