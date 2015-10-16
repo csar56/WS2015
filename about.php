3145bb486cb09dc7637cfde3bafacaf8922459a1
Christoph Sauer <info@sauer-chris.de>
2015-10-16 23:51:23 +0200

    Add About Page

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<title>SauerIT</title>
+	<?php include('php/layout/head.php');?>
+</head>
+<body>
+	<?php include('php/layout/navbar.php');?>
+
+
+
+</body>
+</html>
