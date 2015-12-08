0c521fadc37017f4bd3124745757dd907dd70540
Christoph Sauer <info@sauer-chris.de>
2015-12-08 12:15:31 +0100

    some changes

--- php/layout/head.php
+++ php/layout/head.php
@@ -2,9 +2,10 @@
 	echo '
 			<meta charset="UTF-8" />
 			<link rel="shortcut icon" href="img/sauerit.ico" type="image/x-icon" /> 
-			<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 			<script src="bootstrap/jquery.js"></script>
-			<script src="bootstrap/js/bootstrap.min.js"></script>
+			
+			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 			<link rel="stylesheet" href="css/style.css">
 			';
